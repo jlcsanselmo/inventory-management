56ccdf8256a0e9079cc81495dade40fad792e4e5
jlcsanselmo <jlcsanselmo@gmail.com>
2026-05-03 15:19:39 -0300

    add metodo zod

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { z } from "zod";
 import pg from 'pg';
+
+const productSchema = z.object({
+    name: z.string().min(1, "O nome é obrigatório"),
+    description: z.string().min(1, "A descrição é obrigatória"),
+    price: z.number().positive("O preço deve ser maior que zero"),
+    stock_quantify: z.number().int().nonnegative("O estoque não pode ser negativo")
+});
 
 // Banco de Dados
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
@@ -30,17 +38,25 @@ app.get('/products', async (request, response) => {
 
 app.post('/products', async (request, response) => {
     try {
-        const { name, description, price, stock_quantify } = request.body;
+        const validatedData = productSchema.parse(request.body); // aplicando o metodo zod
+
         const newProduct = await prisma.product.create({
             data: {
-                name,
-                description,
-                price,
-                stock_quantify,
+                name: validatedData.name,
+                description: validatedData.description,
+                price: validatedData.price,
+                stock_quantify: validatedData.stock_quantify,
             },
         });
         response.status(201).json(newProduct);
     } catch (error) {
+
+        if (error instanceof z.ZodError) {
+            return response.status(400).json({
+                message: "Dados inválidos",
+                errors: error.errors
+            });
+        }
         console.error(error);
         response.status(500).json({ error: "Erro ao criar produto" });
     }
@@ -60,19 +76,26 @@ app.delete('/products/:id', async (request, response) => {
 });
 app.put('/products/:id', async (request, response) => {
     try {
+        const validatedData = productSchema.parse(request.body);
+
         const { id } = request.params;
-        const { name, description, price, stock_quantify } = request.body;
         const updatedProduct = await prisma.product.update({
             where: { id: id },
             data: {
-                name,
-                description,
-                price,
-                stock_quantify,
+                name: validatedData.name,
+                description: validatedData.description,
+                price: validatedData.price,
+                stock_quantify: validatedData.stock_quantify,
             },
         });
         response.json(updatedProduct);
     } catch (error) {
+        if (error instanceof z.ZodError) {
+            return response.status(400).json({
+                message: "Dados inválidos",
+                errors: error.errors
+            });
+        }
         console.error(error);
         response.status(500).json({ error: "Erro ao atualizar produto" });
     }
